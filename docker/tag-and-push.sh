77f44bc4052b7244773e2b1ef3297c027b2b39ac
shirey <shirey@pitt.edu>
2023-04-03 11:31:27 -0400

    remove tag-and-push script as it is not needed with multi-arch build

--- docker/tag-and-push.sh
+++
@@ -1,5 +0,0 @@
-#tag the latest local build and push it to DockerHub
-#this will require a "docker login" first with credentials that allow
-#write to the HuBMAP DockerHub org
-docker tag ubkg-neo4j-local:latest hubmap/ubkg-neo4j:latest
-docker push hubmap/ubkg-neo4j:latest
