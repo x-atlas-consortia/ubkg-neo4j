9585ba008f753fe988a418e5eb115fe0820ead32
shirey <shirey@pitt.edu>
2023-09-10 21:37:07 -0400

    don't echo password

--- docker/start.sh
+++ docker/start.sh
@@ -15,7 +15,7 @@ export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:/bin/java::")
 # Get the neo4j username, password, UI port and bolt port from system environment variables.
 # The UI and bolt ports are only needed to echo a message to the user
 NEO4J_USER=${NEO4J_USER}
-NEO4J_PASSWORD=${NEO4J_PASSWORD}
+#NEO4J_PASSWORD=${NEO4J_PASSWORD}
 UI_PORT=${UI_PORT}
 BOLT_PORT=${BOLT_PORT}
 RW_MODE=${RW_MODE}
@@ -39,13 +39,16 @@ echo "Please wait, this may take a minute or two..."
 cp $NEO4J/conf/neo4j.conf $NEO4J/conf/neo4j.conf.secure
 cp $NEO4J/conf/neo4j.conf.noauth $NEO4J/conf/neo4j.conf
 $NEO4J/bin/neo4j start > /dev/null 2>&1
-sleep 5
+until test_cypher_query ; do
+    echo 'Waiting for server to start...'
+    sleep 5
+done
 echo "ALTER USER $NEO4J_USER SET PASSWORD '$NEO4J_PASSWORD'" | $NEO4J/bin/cypher-shell -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" > /dev/null 2>&1
 sleep 5
 if [[ ! `$NEO4J/bin/neo4j stop` ]]; then
   while [[ `$NEO4J/bin/neo4j status` ]]; do
     echo "Waiting for Neo4j to stop and restart..."
-    sleep 1
+    sleep 2
   done;
 fi
 
@@ -90,7 +93,9 @@ NC='\033[0m' # No Color
 echo -e "${RED}***Restarting neo4j server mode..."
 #show the user the local ports where Neo4j can be accessed
 echo -e "***The Neo4j web desktop UI will be available at http://localhost:$UI_PORT"
-echo -e "***The neo4j/bolt interface will be available at neo4j://localhost:$BOLT_PORT and bolt://localhost:$BOLT_PORT ${NC}"
-
+echo -e "***The neo4j/bolt interface will be available at neo4j://localhost:$BOLT_PORT and bolt://localhost:$BOLT_PORT"
+echo -e "***"
+echo -e "***Just a minute or so more..."
+echo -e "***Wait for the \"Started Server\" message ${NC}"
 # Docker requires your command to keep running in the foreground, so start with the console option. Otherwise, it thinks that your applications stops and shuts down the container.
 $NEO4J/bin/neo4j console | grep -v "org\.eclipse\.jetty" | grep -v "Directories in use" | grep -v "usr/src/app/neo4j"
