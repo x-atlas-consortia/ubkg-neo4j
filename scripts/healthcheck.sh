c5a5284991f315abc14ea3b134abcaa64cd27a8b
AlanSimmons <jas971@pitt.edu>
2025-05-30 06:39:20 -0400

    documentation for ubkgbox workflow

--- scripts/healthcheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# -------------------------
-# Unified Biomedical Knowledge Graph (UBKG)
-# Custom health check for ubkg-back-end container.
-
-echo docker exec -it ubkgbox-ubkg-back-end-1 /usr/src/app/neo4j/bin/cypher-shell -u "${NEO4J_USER}" -p "${NEO4J_PASSWORD}" 'match (n) return count(n);' >/dev/null 2>&1
-
